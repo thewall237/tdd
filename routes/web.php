d79f0f37375a946cb734964c53afe5a663e10805
thewall237 <outsidethewall237@gmail.com>
2022-06-16 18:10:59 +0900

    routeを整理

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
+//　ブログ一覧画面
 Route::get('/', [PostController::class, 'index']);
-
+// ブログ詳細画面
 Route::get('posts/{post}', [PostController::class, 'show'])->name('posts.show')->whereNumber('post');
